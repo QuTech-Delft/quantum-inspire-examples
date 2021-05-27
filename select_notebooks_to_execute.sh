378b023b4c18ae2fc838b307ff998cf7fbc83b37
QFer <f.j.grooteman@tudelft.nl>
2021-05-27 19:13:48 +0200

    minimize travis build minutes

--- select_notebooks_to_execute.sh
+++ select_notebooks_to_execute.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#Collect all notebooks to be executed
+# Collect all notebooks to be executed
+# classification_example1_2_data_points.ipynb is excluded because it takes more than 10 minutes (kills CI-pipeline)
 echo "Running notebooks"
 set -ev
 export QI_UNITTEST=ON
-find docs/notebooks/ -type f -name "*.ipynb" -print0 | while IFS= read -r -d $'\0' file;
+find docs/notebooks/ -type f -name "*.ipynb" ! -name 'classification_example1_2_data_points.ipynb' -print0 | while IFS= read -r -d $'\0' file;
 do
     jupyter nbconvert --to notebook --execute "$file"
 done
