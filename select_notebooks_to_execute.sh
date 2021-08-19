56960dbbf41e71ca12dd6a0d0361852b8d747103
QFer <f.j.grooteman@tudelft.nl>
2021-08-19 13:52:38 +0200

    repair set command and solved an error in measurement-error-mitigation notebook

--- select_notebooks_to_execute.sh
+++ select_notebooks_to_execute.sh
@@ -2,7 +2,7 @@
 # Collect all notebooks to be executed
 # classification_example1_2_data_points.ipynb is excluded because it takes more than 10 minutes (kills CI-pipeline)
 echo "Running notebooks"
-# set -ev
+set -e -v
 export QI_UNITTEST=ON
 find docs/notebooks/ -type f -name "*.ipynb" ! -name 'classification_example1_2_data_points.ipynb' -print0 | while IFS= read -r -d $'\0' file;
 do
