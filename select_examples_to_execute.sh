af1ab4abade3576db55b3b86901bf8dd6b31cff1
QFer <46991941+QFer@users.noreply.github.com>
2021-08-19 15:11:06 +0200

    [SDS-NNN] CI Travis 2 GitHub actions (#8)
    * Moved to Github actions instead of Travis

--- select_examples_to_execute.sh
+++ select_examples_to_execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#Collect all python examples to be executed
+# Collect all python examples to be executed
 echo "Running examples"
-set -ev
+set -e -v
 export QI_UNITTEST=ON
 find docs/examples/ -type f -name "example_*.py" -print0 | while IFS= read -r -d $'\0' file;
 do
